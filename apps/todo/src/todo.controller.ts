05ed6680c64edefa7ea9f953fd78fcd2e4359c7d
Naven Prasad <me@naven.digital>
2024-02-26 14:43:56 +0800

    added and tested keycloak

--- apps/todo/src/todo.controller.ts
+++ apps/todo/src/todo.controller.ts
@@ -1,7 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { TodoService } from './todo.service';
-
+import { UseGuards } from '@nestjs/common';
+import {
+  Resource,
+  Roles,
+  Scopes,
+  Unprotected,
+  Public,
+} from 'nest-keycloak-connect';
+import { AuthGuard } from 'nest-keycloak-connect';
 @Controller()
+@UseGuards(AuthGuard)
 export class TodoController {
   constructor(private readonly todoService: TodoService) {}
 
@@ -9,6 +18,7 @@ export class TodoController {
   getHello(): string {
     return this.todoService.getHello();
   }
+
   @Get('todo')
   getTodoList() {
     return this.todoService.getTodoList();
--- apps/todo/src/todo.module.ts
+++ apps/todo/src/todo.module.ts
@@ -1,12 +1,63 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { TodoController } from './todo.controller';
 import { TodoService } from './todo.service';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
+import {
+  AuthGuard,
+  KeycloakConnectModule,
+  ResourceGuard,
+  RoleGuard,
+  KeycloakConnectOptions,
+  KeycloakConnectOptionsFactory,
+  PolicyEnforcementMode,
+  TokenValidation,
+} from 'nest-keycloak-connect';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    KeycloakConnectModule.register({
+      authServerUrl: 'http://localhost:8080', // Make sure the URL is correct
+      realm: 'myrealm',
+      clientId: 'myclient',
+      secret: 'DXVEwa7guEImEvtvgVuyHGrHnqar00bo',
+      policyEnforcement: PolicyEnforcementMode.PERMISSIVE,
+      tokenValidation: TokenValidation.ONLINE,
+    }),
+  ],
   controllers: [TodoController],
-  providers: [TodoService, PrismaService],
+  providers: [
+    TodoService,
+    PrismaService,
+    // These are in order, see https://docs.nestjs.com/guards#binding-guards
+    // for more information
+    //
+    // This adds a global level authentication guard, you can also have it scoped
+    // if you like.
+    //
+    // Will return a 401 unauthorized when it is unable to
+    // verify the JWT token or Bearer header is missing.
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    // This adds a global level resource guard, which is permissive.
+    // Only controllers annotated with @Resource and methods with @Scopes
+    // are handled by this guard.
+    {
+      provide: APP_GUARD,
+      useClass: ResourceGuard,
+    },
+    // New in 1.1.0
+    // This adds a global level role guard, which is permissive.
+    // Used by `@Roles` decorator with the optional `@AllowAnyRole` decorator for allowing any
+    // specified role passed.
+    {
+      provide: APP_GUARD,
+      useClass: RoleGuard,
+    },
+  ],
 })
 export class TodoModule {}
