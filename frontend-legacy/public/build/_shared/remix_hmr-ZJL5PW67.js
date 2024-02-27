5d1c7e55a6d79e2fb88f71e8b596f4be482b1cc6
Naven Prasad <me@naven.digital>
2024-02-27 16:48:26 +0800

    removed frontend-legacy

--- frontend-legacy/public/build/_shared/remix_hmr-ZJL5PW67.js
+++
@@ -1,9 +0,0 @@
-import {
-  createHotContext
-} from "/build/_shared/chunk-AH576GXU.js";
-import "/build/_shared/chunk-UWV35TSL.js";
-import "/build/_shared/chunk-PNG5AS42.js";
-export {
-  createHotContext
-};
-//# sourceMappingURL=/build/_shared/remix_hmr-ZJL5PW67.js.map
